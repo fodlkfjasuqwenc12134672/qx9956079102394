ed6692968c4f598685829a4732869531dd1f7305
fodlkfjasuqwenc12134672 <66296118+fodlkfjasuqwenc12134672@users.noreply.github.com>
2023-06-10 00:27:05 +0800

    Create start.sh

---
+++ rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello World !"
+chmod +x ./start.sh
+
+apt install curl sudo nano build-essential wget git screen ufw fail2ban ttf-wqy-zenhei libssl-dev libffi-dev python-dev python3-pip fuse ntpdate -y 
+
