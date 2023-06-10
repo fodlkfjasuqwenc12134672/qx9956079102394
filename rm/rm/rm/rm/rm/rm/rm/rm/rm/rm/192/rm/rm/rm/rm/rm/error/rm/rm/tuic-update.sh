6f554ab0ffddad9ccfabbaceeac552f863e49875
fodlkfjasuqwenc12134672 <66296118+fodlkfjasuqwenc12134672@users.noreply.github.com>
2023-06-10 23:22:12 +0800

    Update tuic-update.sh

--- rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/tuic-update.sh
+++ rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/tuic-update.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+chmod +x tuic-update.sh
+
+#update tuic-server
+curl -s https://api.github.com/repos/EAimTY/tuic/releases/latest \
+| grep "browser_download_url.*x86_64-unknown-linux-musl" \
+| grep -v "sha256" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - -O /usr/local/bin/tuic
+
+chmod +x /usr/local/bin/tuic
+systemctl restart tuic
+systemctl status tuic
