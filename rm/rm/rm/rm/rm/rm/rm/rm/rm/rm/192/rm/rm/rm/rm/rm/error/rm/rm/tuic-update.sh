10db4ab5a987b7a0062de4818ac6764410e2ff80
fodlkfjasuqwenc12134672 <66296118+fodlkfjasuqwenc12134672@users.noreply.github.com>
2023-06-10 23:39:46 +0800

    Update tuic-update.sh

--- rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/tuic-update.sh
+++ rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/tuic-update.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
 chmod +x tuic-update.sh
+
+blue(){ echo -e "\033[36m\033[01m$1\033[0m";}
+red(){ echo -e "\033[31m\033[01m$1\033[0m";}
+green(){ echo -e "\033[32m\033[01m$1\033[0m";}
+yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
+white(){ echo -e "\033[37m\033[01m$1\033[0m";}
+
 
 systemctl stop tuic
 
@@ -15,3 +21,35 @@ curl -s https://api.github.com/repos/EAimTY/tuic/releases/latest \
 chmod +x /usr/local/bin/tuic
 systemctl restart tuic
 systemctl status tuic
+
+version=$(curl -s https://api.github.com/repos/EAimTY/tuic/releases/latest | grep "tag_name" | grep -v "sha256" | grep -v "github" | cut -d : -f 2,3)
+echo ""
+# echo "TUIC版本成功更新至：${version}"
+white "TUIC版本成功更新至：${version}"
+
+
+
+
+
+
+
+
+
+
+
+
+# echo "curl -s https://api.github.com/repos/EAimTY/tuic/releases/latest \
+# | grep "tag_name" \
+# | grep -v "sha256" \
+# | grep -v "github" \
+# | cut -d : -f 2,3"
+
+
+# curl -s https://api.github.com/repos/EAimTY/tuic/releases/latest \
+# | grep "tag_name" \
+# | grep -v "sha256" \
+# | grep -v "github" \
+# | cut -d : -f 2,3 \
+# | tr -d \" \
+# | wget -qi - -O /usr/local/bin/tuic
+
