88a36be07558c7d028e5b9a3d127008cfdf2df90
fodlkfjasuqwenc12134672 <66296118+fodlkfjasuqwenc12134672@users.noreply.github.com>
2024-01-11 17:53:54 +0800

    Update show-domain-ip.js

--- rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/s/show-domain-ip.js
+++ rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/s/show-domain-ip.js
@@ -1,7 +1,8 @@
 function dnsRequestHandler(request) {
     let domainsToLog = ['binance.com'];
     if (domainsToLog.includes(request.hostname)) {
-        console.log(`已連接到 ${request.hostname}, IP: ${request.ip}`);
+        let message = `已連接到 ${request.hostname}, IP: ${request.ip}`;
+        $notification.post('網絡連接通知', request.hostname, message);
     }
     return null;
 }
