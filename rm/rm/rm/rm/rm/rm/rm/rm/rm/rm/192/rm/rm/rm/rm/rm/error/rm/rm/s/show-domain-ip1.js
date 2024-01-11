d6ca856c61c54eabbaf8609287753d162b0b1459
fodlkfjasuqwenc12134672 <66296118+fodlkfjasuqwenc12134672@users.noreply.github.com>
2024-01-11 18:01:40 +0800

    Update show-domain-ip1.js

--- rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/s/show-domain-ip1.js
+++ rm/rm/rm/rm/rm/rm/rm/rm/rm/rm/192/rm/rm/rm/rm/rm/error/rm/rm/s/show-domain-ip1.js
@@ -1,6 +1,5 @@
 function dnsRequestHandler(request) {
-    let domainsToLog = ['binance.com'];
-    if (domainsToLog.includes(request.hostname)) {
+    if (request.hostname.endsWith('binance.com')) {
         let message = `已連接到 ${request.hostname}, IP: ${request.ip}`;
         console.log(message);  // 將信息記錄到日誌中
         $notification.post('網絡連接通知', request.hostname, message);  // 在App中彈出通知
